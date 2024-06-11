d0796d896694d00cf92d157eb188035854b0f692
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-11 08:44:05 +0600

    bucky python initial commit complete day 10

---
+++ main.py
@@ -0,0 +1,173 @@
+x = 3+4
+
+x = 3 * 20
+
+x = 12 / 4
+
+x = 8 + 2
+
+x = 8 + 2 * 10
+
+x = (8 + 2) * 10
+
+# rounds and drop the decimal
+x = 18 // 4
+
+# modulus
+x = 18 % 4
+
+# ** means calculate the powers
+x = 2 ** 3
+
+# print(x)
+
+# varieble
+tuna = 5
+bacon = 18
+# print(20+tuna)
+# print(bacon/ tuna )
+
+# String needs to be in quotes "" or single quotes ''
+# mix and match single quotes
+
+# 'She said, "what part of the cow is the meatloaf from?" '
+# 'I don\'t think she is 18'
+# use backslash '\' escape character
+
+# print("Hey now brown cow")
+
+# print('C:\Bucky\Desktop\nudePics')
+# r"" raw string formating
+# print(r'C:\Bucky\Desktop\nudePics')
+#
+# firstName = "Bucky "
+# print(firstName + "Roberts")
+#
+# firstName = "McLovin "
+# print(firstName + "Roberts")
+#
+# # string multiplication
+# print(firstName * 5 + "Roberts")
+# computer starts counting from 0
+# user = "Tuna McFish"
+# print(user[0])
+# print(user[5])
+# # negative indexing
+# print(user[-1])
+# print(user[-3])
+# # slice out string
+# print(user[2:7])
+# # start at the beginning
+# print(user[:7])
+# # finish at the end
+# print(user[2:])
+# # Whole entire thing
+# print(user[:])
+# # length of a string len("sdfasdflkjhwe")
+# print(len("sdfasdflkjhwe"))
+# print(len(user))
+
+# python lists
+# players = [29, 58, 66, 71, 87]
+# print(players[2])
+#
+# players[2] = 68
+# print(players)
+#
+# new_players = players + [90, 91, 98]
+# print(new_players)
+# players.append(120)
+# print(players)
+# # slicing a list in python similar to slicing a string
+# print(players[:2])
+# # changing multiple values at once in a list
+# players[:2] = [0, 0]
+# print(players)
+# # slicing a list and removing items
+# players[:2] = []
+# print(players)
+# # entire list empty
+# players[:] = []
+
+# pycharm is bucky's favorite ide and he has the professional edition
+# project is where all the files are stored
+# many files it may have
+# .py is the extension
+
+# print("this works")
+# cool program begins
+# cardboard is not allowed to be thrown away I guess
+# lets learn if statement
+# age = 13
+# if age < 21:
+#     print('No beer for you!')
+# elif
+
+# name = "Tommy D"
+# if name == "Bucky":
+#     print("Hey there Bucky")
+# elif name == "Lucy":
+#     print("What up Lucedawg")
+# elif name == "Sammy":
+#     print("What up Slammy")
+# else:
+#     print("Please sign up for the site!")
+
+# for loop bucky
+# foods = ['bacon', 'tune', 'ham', 'sausages', 'beef']
+# # you can also slice the list for looping through
+# for f in foods[:2]:
+#     print(f)
+#     print(len(f))
+
+# for x in range(10):
+#     print(x)
+# range(start, end - 1, increment)
+# for x in range(5, 40, 5):
+#     print(x)
+
+# buttcrack = 5
+#
+# while buttcrack < 10:
+#     print(buttcrack)
+#     buttcrack += 1
+
+# break and continue
+# ok this program finds a magic number
+# single line comments
+'''
+multi line comment
+'''
+
+# print number and string in the same line
+# print(9, " Bucky")
+# magicNumber = 26
+#
+# for n in range(101):
+#     if n is magicNumber:
+#         print(n, " is the magic number!")
+#         break
+#     else:
+#         print(n)
+# for i in range(1, 101):
+#     if i%4 == 0:
+#         print(i)
+
+# Continue
+numbersTaken = [2, 5, 12, 13, 17]
+
+print("Here are the numbers that are still available")
+
+for n in range(1, 20):
+    if n in numbersTaken:
+        continue
+    print(n)
+
+
+
+
+
+
+
+
+
